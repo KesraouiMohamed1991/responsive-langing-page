77145c37b51275e2eb3f6765ce39e7cba8bcaf3a
kesraoui mohamed <kesraoui15003@gmail.com>
2023-05-13 23:17:26 +0200

    this is the content

---
+++ app.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.icon');
+const menu = document.querySelector('.drop-down-menu');
+
+toggleBtn.addEventListener("click", function () {
+    
+    menu.classList.toggle('active')
+})
+console.log(menu)
